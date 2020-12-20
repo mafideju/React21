07d84e01b1d2826f4fef67d43347882dff83b3d1
Marcio Mafideju Rodrigues <mafideju@outlook.com>
2020-12-20 19:25:57 -0300

    feat(app): refreshing

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import { shallow } from 'enzyme';
 
@@ -8,19 +7,20 @@ import CommentBox from './Components/CommentBox/CommentBox.jsx';
 import CommentList from './Components/CommentList/CommentList';
 
 describe('MAIN APP', () => {
+  // beforeEach(() => {
+  //   const wrapper = shallow(<App />);
+  // });
+  
   test('renders component CommentBox', () => {
     const wrapper = shallow(<App />);
 
     expect(wrapper.find(CommentBox).length).toEqual(1);
   });
 
-  test('renders CommentList', () => {
+  test('renders component CommentList', () => {
     const wrapper = shallow(<App />);
 
     expect(wrapper.find(CommentList).length).toEqual(1);
   });
   
-  // beforeEach(() => {
-  //   const wrapper = shallow(<App />);
-  // });
 });
